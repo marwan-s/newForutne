91dd6c59fa8ca80e107bf2c6379c934ae5e962c5
marwan-s <37839883+marwan-s@users.noreply.github.com>
2025-06-01 13:13:57 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,52 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages] });
+
+const fortunes = [
+   "You will have a great day!",
+    "Something unexpected will happen.",
+    "Your code will run on the first try!",
+    "A surprise is coming your way.",
+    "Don't forget to take breaks!",
+    "Your coffee might spill today—be careful!",
+    "An old bug may return to haunt you.",
+    "You’ll receive good news before the day ends.",
+    "Someone is thinking of you right now.",
+    "You might forget something important—double check!",
+    "Your next idea could change everything.",
+    "Your phone battery is probably lower than you think.",
+    "You're doing better than you realize.",
+    "An awkward moment may become a funny memory.",
+    "A minor inconvenience will test your patience.",
+    "Today is the day you make progress.",
+    "You might laugh at the wrong moment—oops.",
+    "A small victory is coming your way.",
+    "You may get distracted, but you'll recover fast.",
+    "An update might break something—save your work!",
+    "You’ll have a random moment of inspiration.",
+    "Something you've been avoiding will appear.",
+    "You're going to crush your next task.",
+    "There may be delays—don’t let them ruin your vibe.",
+    "You’ll overhear something funny today.",
+    "One tab too many might crash your browser.",
+    "Today’s mood: cautiously optimistic.",
+    "You may accidentally say what you're thinking.",
+    "Someone will compliment your work.",
+    "You might get blamed for something you didn’t do.",
+    "You’ll discover something surprisingly useful.",
+    "A challenge will reveal your strength.",
+    "You may step on something wet with socks on."
+];
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', message => {
+  if (message.author.bot) return;
+  if (message.content === '--new333') {
+    const fortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+    message.channel.send(fortune);
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
